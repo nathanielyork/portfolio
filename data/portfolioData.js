3bf4fc7aedb9c672e9c41194499e1dc85336cb6e
nathanielyork <nathsenou@gmail.com>
2020-12-17 14:31:32 +0000

    Add files via upload

---
+++ data/portfolioData.js
@@ -0,0 +1,10 @@
+export const portfolioData = [
+  {
+    id: 1,
+    name: 'ipsum.com',
+    languages: ['no','no'],
+    languagesIcons: ['fab fa-js','fab fa-css3-alt'],
+    source: 'http://www.github.com',
+    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
+    picture: './media/project1.JPG'
+  }]
